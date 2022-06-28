44cc8cd8e876ae41e2fb27a5826e7cfba6fa3068
Pat <patrick.turner@nhs.net>
2022-06-28 15:32:31 +0100

    first pass at badges
    use NHS tag macro

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,24 +1,4 @@
-const dateConverter = require('../../lib/_javascripts/dateConverter')
-const { DateTime } = require('luxon')
-
-const defaultReviewDuration = { years: 1}
-const today = DateTime.local()
-const reviewDurationLookup = {
-    ANNUAL: defaultReviewDuration
-}
 module.exports = {
-    review: data => {
-        if (data.review) {
-            let lastDate = data.review.last_reviewed_date || data.issuesheet.approval_date || today
-            console.log(lastDate)
-            lastDate = dateConverter(lastDate)
-            let cycle = data.review.review_cycle
-            let duration = reviewDurationLookup[cycle] || cycle || defaultReviewDuration;
-            let nextDate = lastDate.plus(duration).toISO();
-            return {
-                last_reviewed_date: lastDate,
-                next_review_date: nextDate
-            }
-        }
-    }
+    review: require('../../lib/_computed/reviewDate'),
+    badges: require('../../lib/_computed/badges')
 };
