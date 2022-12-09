62e7cba6025a46164ebdf8b47e746aa398c911e6
Archie <archie.wort@nhs.net>
2022-12-09 12:00:31 +0000

    update badges test to match function naming refactor

--- tests/badges.test.js
+++ tests/badges.test.js
@@ -1,4 +1,4 @@
-const checkDataStatus = require('../lib/_computed/badges');
+const addStatusBadges = require('../lib/_computed/badges');
 
 describe('badge status unit tests', () => {
     // default fixture
@@ -8,7 +8,7 @@ describe('badge status unit tests', () => {
     };
 
     it('should return grey and DRAFT when data has DRAFT status and no review', () => {
-        expect(checkDataStatus(data)).toEqual([{
+        expect(addStatusBadges(data)).toEqual([{
             text: "DRAFT",
             style: "nhsuk-tag--grey"
         }]);
@@ -16,7 +16,7 @@ describe('badge status unit tests', () => {
     
     it('should return yellow and REVIEW when data has review and DRAFT status', () => {
         data.review.requires_review = true;
-        expect(checkDataStatus(data)).toEqual([{
+        expect(addStatusBadges(data)).toEqual([{
             text: "REVIEW",
             style: "nhsuk-tag--yellow"
         }]);
@@ -24,7 +24,7 @@ describe('badge status unit tests', () => {
 
     it('should return yellow and REVIEW when data has review and FINAL status', () => {
         data.status = "FINAL";
-        expect(checkDataStatus(data)).toEqual([{
+        expect(addStatusBadges(data)).toEqual([{
             text: "REVIEW",
             style: "nhsuk-tag--yellow"
         }]);
@@ -33,13 +33,13 @@ describe('badge status unit tests', () => {
     it('will return empty if FINAL status and no review', () => {
         data.status = "FINAL";
         data.review.requires_review = false;
-        expect(checkDataStatus(data)).toEqual([]);
+        expect(addStatusBadges(data)).toEqual([]);
     });
 
     it('will retain user-defined badges', () => {
         userBadges = [{text: "my-badge"}]
         data.badges = userBadges
-        expect(checkDataStatus(data)).toEqual(userBadges);
+        expect(addStatusBadges(data)).toEqual(userBadges);
     });
 });
 
