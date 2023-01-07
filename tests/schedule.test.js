f4e24a4be113fd2507e5399c7c0952a4a1db707f
Archie <archie.wort@nhs.net>
2023-01-07 14:57:22 +0000

    Updated code in-line with PR review

--- tests/schedule.test.js
+++ tests/schedule.test.js
@@ -1,152 +1,183 @@
-const getReleaseAdoptionSchedule = require('../lib/_computed/schedule');
-const { DateTime } = require('luxon');
+const getReleaseAdoptionSchedule = require("../lib/_computed/schedule");
+const { DateTime } = require("luxon");
 
-describe('releaseAdoptionSchedule empty dataset tests', () => {
-    let data = {
-        title: "Release adoption strategy"
-    };
-    it('should return empty releaseAdoptionSchedule if data has no releaseAdoptionSchedule', () => {
-        expect(getReleaseAdoptionSchedule(data)).toEqual({});
-    });
+describe("releaseAdoptionSchedule empty dataset tests", () => {
+  it("should return empty releaseAdoptionSchedule if data has no releaseAdoptionSchedule", () => {
+    const data = { title: "Release adoption strategy" };
+    expect(getReleaseAdoptionSchedule(data)).toEqual({});
+  });
 
-    data = {
-        releaseAdoptionSchedule: {}
-    };
-    it('should return empty releaseAdoptionSchedule if data has empty releaseAdoptionSchedule', () => {
-        expect(getReleaseAdoptionSchedule(data)).toEqual({});
-    });
+  it("should return empty releaseAdoptionSchedule if data has empty releaseAdoptionSchedule", () => {
+    const data = { releaseAdoptionSchedule: {} };
+    expect(getReleaseAdoptionSchedule(data)).toEqual({});
+  });
 });
 
-describe('multiple technologies with single versions releaseAdoptionSchedule', () => {
+describe("technology phase tests - releaseAdoptionSchedule", () => {
+  it("should return pending as default phase", () => {
     const data = {
-        releaseAdoptionSchedule: {
-            "node.js": [{
-                release: "",
-                adopt: "",
-                decommission: ""
-            }],
-            "java": [{
-                release: "",
-                adopt: "",
-                decommission: ""
-            }]
-        }
-    };
-
-    // releaseAdoptionSchedule array of data returned 
-    const defaultExpectedResult = {
-        "node.js": [{
+      releaseAdoptionSchedule: {
+        java: [
+          {
             release: "",
             adopt: "",
             decommission: "",
-            phase: "pending"
-        }],
-        "java": [{
-            release: "",
-            adopt: "",
-            decommission: "",
-            phase: "pending"
-        }]
+          },
+        ],
+      },
+    };
+    const result = getReleaseAdoptionSchedule(data);
+    expect(result.java).toStrictEqual([
+      {
+        adopt: "",
+        decommission: "",
+        phase: "pending",
+        release: "",
+      },
+    ]);
+  });
+
+  it("should return assess phase when java release date is in the past", () => {
+    const releaseDate = DateTime.local().startOf("day").minus({ years: 1 });
+    const data = {
+      releaseAdoptionSchedule: {
+        java: [{ release: releaseDate }],
+      },
     };
 
-    it('should return pending as default phase', () => {
-        const result = getReleaseAdoptionSchedule(data);
-        expect(result).toEqual(defaultExpectedResult);
-    });
+    const result = getReleaseAdoptionSchedule(data);
+    expect(result.java).toStrictEqual([
+      {
+        release: releaseDate,
+        phase: "assess",
+      },
+    ]);
+  });
 
-    const releaseDate = DateTime.local().startOf('day').minus({ years: 1 });
-    it('should return assess phase when java release date is in the past', () => {
-        data.releaseAdoptionSchedule.java = [{
-            release: releaseDate
-        }];
-        
-        const result = getReleaseAdoptionSchedule(data);
-        expect(result.java).toEqual([{
-            release: releaseDate,
-            phase: "assess"
-        }]);
-    });
-
-    const adoptDate = DateTime.local().startOf('day').minus({ months: 6 });
-    it('should return adopt as phase when java adopt date is in the past', () => {
-        data.releaseAdoptionSchedule.java = [{
-            release: releaseDate,
-            adopt: adoptDate
-        }];
-        
-        const result = getReleaseAdoptionSchedule(data);
-        expect(result.java).toEqual([{
+  it("should return adopt as phase when java adopt date is in the past", () => {
+    const releaseDate = DateTime.local().startOf("day").minus({ years: 1 });
+    const adoptDate = DateTime.local().startOf("day").minus({ months: 6 });
+    const data = {
+      releaseAdoptionSchedule: {
+        java: [
+          {
             release: releaseDate,
             adopt: adoptDate,
-            phase: "adopt"
-        }]);
-    });
+          },
+        ],
+      },
+    };
 
+    const result = getReleaseAdoptionSchedule(data);
+    expect(result.java).toStrictEqual([
+      {
+        release: releaseDate,
+        adopt: adoptDate,
+        phase: "adopt",
+      },
+    ]);
+  });
 });
 
-describe('multiple versions test: same technology releaseAdoptionSchedule', () => {
+it("should be able to handle multiple technologies - settig default phase to pending", () => {
+  const data = {
+    releaseAdoptionSchedule: {
+      java: [{ release: "" }],
+      node: [{ release: "" }],
+      patlang: [{ release: "" }],
+    },
+  };
+  const result = getReleaseAdoptionSchedule(data);
+  expect(result).toStrictEqual({
+    java: [{ phase: "pending", release: "" }],
+    node: [{ phase: "pending", release: "" }],
+    patlang: [{ phase: "pending", release: "" }],
+  });
+});
+
+it("should be able to handle multiple technologies - with different values", () => {
+  const nodeReleaseDate = DateTime.local().startOf("day").minus({ years: 1 });
+  const patlangAdoptDate = DateTime.local().startOf("day").minus({ years: 1 });
+  const data = {
+    releaseAdoptionSchedule: {
+      java: [{ release: "" }],
+      node: [{ release: nodeReleaseDate }],
+      patlang: [{ release: "", adopt: patlangAdoptDate }],
+    },
+  };
+  const result = getReleaseAdoptionSchedule(data);
+  expect(result).toStrictEqual({
+    java: [{ phase: "pending", release: "" }],
+    node: [{ phase: "assess", release: nodeReleaseDate }],
+    patlang: [{ phase: "pending", release: "", adopt: patlangAdoptDate }],
+  });
+});
+
+describe("multiple versions test: same technology - releaseAdoptionSchedule", () => {
+  it("should return correct phase cycles for multiple version fixture data", () => {
     // fixture for multiple versions of the same tech
-    const today = DateTime.local().startOf('day');
+    const today = DateTime.local().startOf("day");
     const initialReleaseDate = today.minus({ years: 10 });
     // fixture for testing purposes only
     const data = {
-        releaseAdoptionSchedule: {
-            "java": [{
-                version: 21,
-                release: today,
-                adopt: today.plus({ months: 6 })
-            },
-            {
-                version: 17.5,
-                release: today.minus({ years: 2 }),
-                adopt: today,
-                decommission: today.plus({ years: 2 })
-            },
-            {
-                version: 17,
-                release: today.minus({ years: 5 }),
-                adopt: today.minus({ years: 3 }),
-                decommission: today.plus({ years: 2 })
-            },
-            {
-                version: 8,
-                release: initialReleaseDate,
-                adopt: initialReleaseDate.plus({ years: 5 }),
-                decommission: initialReleaseDate.plus({ years: 7 })
-            }]
-        }
+      releaseAdoptionSchedule: {
+        java: [
+          {
+            version: 21,
+            release: today,
+            adopt: today.plus({ months: 6 }),
+          },
+          {
+            version: 17.5,
+            release: today.minus({ years: 2 }),
+            adopt: today,
+            decommission: today.plus({ years: 2 }),
+          },
+          {
+            version: 17,
+            release: today.minus({ years: 5 }),
+            adopt: today.minus({ years: 3 }),
+            decommission: today.plus({ years: 2 }),
+          },
+          {
+            version: 8,
+            release: initialReleaseDate,
+            adopt: initialReleaseDate.plus({ years: 5 }),
+            decommission: initialReleaseDate.plus({ years: 7 }),
+          },
+        ],
+      },
     };
-
-    const expectedResult = [{
+    const expectedResult = [
+      {
         version: 21,
         release: today,
         adopt: today.plus({ months: 6 }),
-        phase: "assess"
-    },
-    {
+        phase: "assess",
+      },
+      {
         version: 17.5,
         release: today.minus({ years: 2 }),
         adopt: today,
         decommission: today.plus({ years: 2 }),
-        phase: "adopt"
-    },
-    {
+        phase: "adopt",
+      },
+      {
         version: 17,
         release: today.minus({ years: 5 }),
         adopt: today.minus({ years: 3 }),
         decommission: today.plus({ years: 2 }),
-        phase: "deprecate"
-    },
-    {
+        phase: "deprecate",
+      },
+      {
         version: 8,
         release: initialReleaseDate,
         adopt: initialReleaseDate.plus({ years: 5 }),
         decommission: initialReleaseDate.plus({ years: 7 }),
-        phase: "decommission"
-    }];
-
-    it('should return correct phase cycles for multiple version fixture data', () => {
-        const result = getReleaseAdoptionSchedule(data);
-        expect(result.java).toEqual(expectedResult);
-    });
+        phase: "decommission",
+      },
+    ];
+    const result = getReleaseAdoptionSchedule(data);
+    expect(result.java).toMatchObject({ ...expectedResult });
+  });
 });
