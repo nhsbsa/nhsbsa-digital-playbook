3d99660edf72692fa47443c891543a1d07790e93
Archie <archie.wort@nhs.net>
2023-01-28 11:45:51 +0000

    chore(eslint) prettier styling updates from test:eslint

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,5 +1,5 @@
 module.exports = {
-    review: require('../../lib/_computed/reviewData'),
-    badges: require('../../lib/_computed/badges'),
-    schedule: require('../../lib/_computed/schedule')
+  review: require("../../lib/_computed/reviewData"),
+  badges: require("../../lib/_computed/badges"),
+  schedule: require("../../lib/_computed/schedule"),
 };
