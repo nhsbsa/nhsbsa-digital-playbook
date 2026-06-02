f157aaaeec687e2c1080020a88198950aec5cfee
Pat Turner <pattu@nhsbsa.nhs.uk>
2026-06-02 10:58:15 +0100

    perf fix - git last modified
    git last modified call is slow on Node 22 and Mac
    only execute in prod build - GH actions fixed

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,3 +1,8 @@
 module.exports = {
-  date: 'git Last Modified',
+  // 'git Last Modified' spawns a git subprocess per template (~150ms each).
+  // In dev, use the fast filesystem mtime instead.
+  date:
+    process.env.NODE_ENV === 'production'
+      ? 'git Last Modified'
+      : 'Last Modified',
 };
